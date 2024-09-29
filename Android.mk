4d61c1a9d5e4f8d4ab9cddb39f3089360f4374b8
schwienernitzel <pfelix0803@gmail.com>
2024-09-29 14:12:08 +0000

    mozart: Initial commit
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter mozart,$(TARGET_DEVICE)),)
+
+endif
---
+++ mozart-vendor-blobs.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_COPY_FILES += \
+	$(call find-copy-subdir-files,*,vendor/huawei/mozart/proprietary/,system/)
